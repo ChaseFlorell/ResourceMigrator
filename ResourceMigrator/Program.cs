b71d39dbd16e7fd3642fc3978cbcbb90de80dc75
Chase Florell <chase.florell@gmail.com>
2014-05-19 14:53:46 -0600

    now supports iOS

--- ResourceMigrator/Program.cs
+++ ResourceMigrator/Program.cs
@@ -10,13 +10,16 @@ namespace ResourceMigrator
         {
             var sourceDirString = args[0];
             var androidTargetDir = args[1];
-
+            var touchTargetDir = args[2];
+            var touchNameSpace = args.Length >= 3 ? args[3] : "FutureState.AppCore.Touch";
+            
             var sourceDir = new DirectoryInfo(sourceDirString);
 
             foreach (var sourceFile in sourceDir.GetFiles("*.resx"))
             {
                 var resources = sourceFile.LoadResources();
                 Droid.WriteToTarget(sourceFile, androidTargetDir, resources);
+                Touch.WriteToTarget(sourceFile, touchTargetDir, resources, touchNameSpace);
             }
         }
     }
