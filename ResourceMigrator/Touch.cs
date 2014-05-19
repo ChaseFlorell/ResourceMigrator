b3693387334ad93dd9ff61b7ed0c97c90cc13275
Chase Florell <chase.florell@gmail.com>
2014-05-19 16:09:58 -0600

    Fixed Alpha transform

--- ResourceMigrator/Touch.cs
+++ ResourceMigrator/Touch.cs
@@ -44,18 +44,9 @@ namespace " + _touchNameSpace + @"
     internal static class CustomUIColor
     {
 
-        private static MonoTouch.UIKit.UIColor FromHexString(string hexValue, float alpha = 1.0f)
+        private static MonoTouch.UIKit.UIColor FromHexString(string hexValue)
         {
             var colorString = hexValue.Replace(""#"", """");
-            if (alpha > 1.0f)
-            {
-                alpha = 1.0f;
-            }
-            else if (alpha < 0.0f)
-            {
-                alpha = 0.0f;
-            }
-
             float red, green, blue;
 
             switch (colorString.Length)
@@ -65,16 +56,23 @@ namespace " + _touchNameSpace + @"
                         red = Convert.ToInt32(string.Format(""{0}{0}"", colorString.Substring(0, 1)), 16) / 255f;
                         green = Convert.ToInt32(string.Format(""{0}{0}"", colorString.Substring(1, 1)), 16) / 255f;
                         blue = Convert.ToInt32(string.Format(""{0}{0}"", colorString.Substring(2, 1)), 16) / 255f;
-                        return MonoTouch.UIKit.UIColor.FromRGBA(red, green, blue, alpha);
+                        return MonoTouch.UIKit.UIColor.FromRGB(red, green, blue);
                     }
                 case 6: // #RRGGBB
                     {
                         red = Convert.ToInt32(colorString.Substring(0, 2), 16) / 255f;
                         green = Convert.ToInt32(colorString.Substring(2, 2), 16) / 255f;
                         blue = Convert.ToInt32(colorString.Substring(4, 2), 16) / 255f;
+                        return MonoTouch.UIKit.UIColor.FromRGB(red, green, blue);
+                    }
+                case 8: // #AARRGGBB
+                    {
+                        var alpha = Convert.ToInt32(colorString.Substring(0, 2), 16)/255f;
+                        red = Convert.ToInt32(colorString.Substring(2, 2), 16)/255f;
+                        green = Convert.ToInt32(colorString.Substring(4, 2), 16) / 255f;
+                        blue = Convert.ToInt32(colorString.Substring(6, 2), 16) / 255f;
                         return MonoTouch.UIKit.UIColor.FromRGBA(red, green, blue, alpha);
                     }
-
                 default:
                     throw new ArgumentOutOfRangeException(string.Format(""Invalid color value {0} is invalid. It should be a hex value of the form #RBG, #RRGGBB"", hexValue));
 
